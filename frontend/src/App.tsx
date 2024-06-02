1cd6e2acaf968b6163769f4619f64f39d185f971
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2024-06-02 21:03:04 +0200

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,32 +2,9 @@ import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Navigate, RouterProvider, createHashRouter } from 'react-router-dom';
-import { BasicAppShell } from './components/BasicAppShell';
-import Demo from './components/Demo';
-import NotFound from './components/NotFound/NotFound';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/router';
 import { theme } from './theme';
-
-/* Using HashRouter for routing to work in production!!! */
-const router = createHashRouter([
-  {
-    element: <BasicAppShell />,
-    children: [
-      {
-        path: '/',
-        element: <Navigate to='/home' />,
-      },
-      {
-        path: '/home',
-        element: <Demo />,
-      },
-      {
-        path: '*',
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
 
 function App() {
   const queryClient = new QueryClient();
