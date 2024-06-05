384a7909495a7c5ed7430ff8e7074bb932657dc6
Ammar Ansari <iammaransari@gmail.com>
2024-06-06 00:31:15 +0200

    Responsive header and footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { AppShell, Text } from '@mantine/core';
 export function Footer() {
   return (
     <AppShell.Footer p='xs' h='50'>
-      <Text ta='center' c='gray'>
+      <Text ta='center' c='gray' className='text-xs sm:text-base'>
         Fulda University Software Engineering Project Summer 2024. For
         demonstration purposes only.
       </Text>
