4260f33a115adf29f739b33bd18c714d76ae6f12
Ammar Ansari <iammaransari@gmail.com>
2024-06-06 13:48:30 +0200

    Responsive header and footer (#48)

--- frontend/src/components/SearchTutor.tsx
+++ frontend/src/components/SearchTutor.tsx
@@ -1,29 +1,36 @@
-import { Select } from '@mantine/core';
-import { useLocation, useNavigate } from 'react-router';
+import { Autocomplete } from '@mantine/core';
+import { IconSearch } from '@tabler/icons-react';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router';
+
+const largeData = Array(100)
+  .fill(0)
+  .map((_, index) => `Subject ${index + 1}`);
 
 export function SearchTutor() {
   const navigate = useNavigate();
-  const location = useLocation();
 
-  // Parse the query parameters
-  const params = new URLSearchParams(location.search);
-  const subject = params.get('subject') || '';
-
-  const handleSelectChange = (value: string | null) => {
-    if (!value) {
-      navigate('/search');
-      return;
-    }
-    navigate(`/search?subject=${value}`);
-  };
+  const handleSubmit = useCallback(
+    (value: string | null) => {
+      if (!value) {
+        return;
+      }
+      navigate(`/search?subject=${value}`);
+    },
+    [navigate]
+  );
 
   return (
-    <Select
+    <Autocomplete
       placeholder='Search tutors by subject'
-      data={['Physics', 'Computer Science', 'Mathematics', 'Chemistry']}
-      searchable
-      value={subject}
-      onChange={handleSelectChange}
+      leftSection={
+        <IconSearch className='w-3 h-3 sm:w-4 sm:h-4' stroke={1.5} />
+      }
+      data={largeData}
+      onOptionSubmit={handleSubmit}
+      classNames={{
+        input: 'w-40 sm:w-64 text-xs sm:text-base',
+      }}
     />
   );
 }
