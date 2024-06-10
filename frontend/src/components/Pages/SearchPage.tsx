ba129244bde00a0c41eead9bd942b08a41b33561
Ammar Ansari <iammaransari@gmail.com>
2024-06-10 21:53:01 +0200

    Filters for Mobile screen (#61)
    * Filters for Mobile screen
    
    * fix icon visibility

--- frontend/src/components/Pages/SearchPage.tsx
+++ frontend/src/components/Pages/SearchPage.tsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
+import { useLocation } from 'react-router';
 import {
+  ActionIcon,
   Center,
   Container,
   Divider,
+  Drawer,
   Flex,
+  Group,
   Loader,
   Stack,
   Text,
 } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
+import { IconAdjustments } from '@tabler/icons-react';
 import { getTutors } from '../../service/searchApi';
 import TutorCard from '../TutorCard';
 import { Filters } from '../Filters';
-import { useLocation } from 'react-router';
 
 export function SearchPage() {
+  const [drawerOpened, setDrawerOpened] = useState(false);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const {
@@ -39,6 +45,31 @@ export function SearchPage() {
         <Divider orientation='vertical' visibleFrom='sm' className='mx-4' />
 
         <Stack className='flex-grow'>
+          {/* Filters for mobile screen */}
+          <Group hiddenFrom='sm'>
+            <ActionIcon
+              variant='default'
+              aria-label='Filters in drawer'
+              size='sm'
+              bd={0}
+              onClick={() => setDrawerOpened(true)}
+            >
+              <IconAdjustments stroke={1} />
+            </ActionIcon>
+            <Drawer
+              opened={drawerOpened}
+              onClose={() => setDrawerOpened(false)}
+              title={
+                <Text size='lg' fw={500} hiddenFrom='sm'>
+                  Filters
+                </Text>
+              }
+              padding='md'
+              size='sm'
+            >
+              <Filters onSubmit={() => setDrawerOpened(false)} />
+            </Drawer>
+          </Group>
           {isLoading && (
             <Center style={{ height: 'calc(100vh - 120px)' }}>
               <Loader type='bars'></Loader>
