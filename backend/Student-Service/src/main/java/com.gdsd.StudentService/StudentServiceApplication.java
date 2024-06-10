3190446006111a82dfb73c29bb34e504c2e2a434
Rick <adamcooldude38@gmail.com>
2024-06-10 18:28:41 +0200

    Init of student module Pipieline is still not present

---
+++ backend/Student-Service/src/main/java/com.gdsd.StudentService/StudentServiceApplication.java
@@ -0,0 +1,16 @@
+package com.gdsd.StudentService;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class StudentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentServiceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() { return new ModelMapper(); }
+}
