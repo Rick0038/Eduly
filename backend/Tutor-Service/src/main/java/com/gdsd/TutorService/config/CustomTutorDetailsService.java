bfd1bc4d4d8728c4744f5b344fbfcd740bed879c
Aashay Markale <aashaymarkale259@gmail.com>
2024-06-11 14:58:22 +0200

    Added CustomTutorDetailsService which is needed to load the Tutor's auth details from database

---
+++ backend/Tutor-Service/src/main/java/com/gdsd/TutorService/config/CustomTutorDetailsService.java
@@ -0,0 +1,37 @@
+package com.gdsd.TutorService.config;
+
+import com.gdsd.TutorService.exception.GenericTutorException;
+import com.gdsd.TutorService.model.Tutor;
+import com.gdsd.TutorService.repository.TutorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomTutorDetailsService implements UserDetailsService {
+
+    @Autowired
+    private TutorRepository tutorRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Tutor tutor = tutorRepository.findByEmail(email).
+                orElseThrow(() ->
+                        new GenericTutorException("Tutor with email" + email + " not found.",
+                                HttpStatus.NOT_FOUND));
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        return new User(tutor.getEmail(), tutor.getPassword(), authorities);
+
+    }
+}
