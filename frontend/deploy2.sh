077d9c2973a76c8e20368737d30e7eb9449710a4
Keshav Upadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2024-06-16 00:20:28 +0200

    update

--- frontend/deploy2.sh
+++ frontend/deploy2.sh
@@ -1,5 +1,5 @@
 export APP_API_URL="http://20.224.153.213:5001"
-export APP_WS_URL='ws://20.224.153.213:5001'
+export APP_WS_URL="ws://20.224.153.213:5001"
 
 # git checkout dev && git pull origin dev
 
