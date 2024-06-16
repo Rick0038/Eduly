60b2e2da03f6c52a94eb705db60ae9828e3e9cd6
Advaith H L <niceadvaith@gmail.com>
2024-06-16 20:49:43 +0200

    Add homepage features section

--- frontend/src/components/Pages/HomePage.tsx
+++ frontend/src/components/Pages/HomePage.tsx
@@ -1,7 +1,11 @@
 import { Hero } from '../HomepageHero';
+import { FeaturesCards } from '../FeaturesCards';
 
 export function HomePage() {
   return (
-    <Hero />
+    <>
+      <Hero />
+      <FeaturesCards />
+    </>
   );
 }
