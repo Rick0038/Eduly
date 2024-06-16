986b9645e86b2f2474356824c330429fe3372182
Aashay Markale <aashaymarkale259@gmail.com>
2024-06-16 11:03:00 +0200

    Added methods existsByEmail in both student and tutor repositories to check if the email is already registered while doing registration

--- backend/Tutor-Service/src/main/java/com/gdsd/TutorService/repository/StudentRepository.java
+++ backend/Tutor-Service/src/main/java/com/gdsd/TutorService/repository/StudentRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
     Optional<Student> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
