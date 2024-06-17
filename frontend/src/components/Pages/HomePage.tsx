8f767f2a948eed541f54b9dbddf255164de890c6
Advaith H L <niceadvaith@gmail.com>
2024-06-18 00:02:09 +0200

    Refactor home page components

--- frontend/src/components/Pages/HomePage.tsx
+++ frontend/src/components/Pages/HomePage.tsx
@@ -1,8 +1,8 @@
-import { Hero } from '../HomepageHero';
-import { FeaturesCards } from '../FeaturesCards';
-import { UserCounts } from '../UserCounts';
-import { Testimonials } from '../Testimonials';
-import { Tutorad } from '../Tutorad';
+import { Hero } from './HomePageComponents/HomepageHero';
+import { FeaturesCards } from './HomePageComponents/FeaturesCards';
+import { UserCounts } from './HomePageComponents/UserCounts';
+import { Testimonials } from './HomePageComponents/Testimonials';
+import { Tutorad } from './HomePageComponents/Tutorad';
 
 export function HomePage() {
   return (
