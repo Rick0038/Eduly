3ab6af7b774b72b324031a44084626296d3d2ee9
Aashay Markale <aashaymarkale259@gmail.com>
2024-06-23 17:45:20 +0200

    Tutor Search API updated

--- backend/Tutor-Service/src/main/java/com/gdsd/TutorService/repository/SessionRepository.java
+++ backend/Tutor-Service/src/main/java/com/gdsd/TutorService/repository/SessionRepository.java
@@ -2,10 +2,24 @@ package com.gdsd.TutorService.repository;
 
 import com.gdsd.TutorService.model.Session;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface SessionRepository extends JpaRepository<Session, Integer> {
     Optional<List<Session>> findByTutorIdAndStatusOrderByDateAscStartTimeAsc(Integer tutorId, String status);
+    Boolean existsByTutorIdAndStatusAndDay(Integer tutorId, String status, String day);
+    @Query("SELECT CASE WHEN COUNT(s) > 0 THEN true ELSE false END FROM Session s " +
+            "WHERE s.tutorId = :tutorId AND s.status = 'FREE' AND s.startTime >= :startTime")
+    Boolean existsByTutorIdAndStatusAndStartTimeGreaterThanEqual(
+            @Param("tutorId") Integer tutorId,
+            @Param("startTime") LocalTime startTime);
+    @Query("SELECT CASE WHEN COUNT(s) > 0 THEN true ELSE false END FROM Session s " +
+            "WHERE s.tutorId = :tutorId AND s.status = 'FREE' AND s.endTime <= :endTime")
+    Boolean existsByTutorIdAndStatusAndEndTimeLessThanEqual(
+            @Param("tutorId") Integer tutorId,
+            @Param("endTime") LocalTime endTime);
 }
