19316bc917dfa2bb6fc737abf707de703e19e00e
Advaith H L <niceadvaith@gmail.com>
2024-06-26 21:15:27 +0200

    Add AdminService

---
+++ frontend/src/service/AdminService.ts
@@ -0,0 +1,26 @@
+import { httpService } from './HTTPService';
+
+interface TutorContents {
+  content: TutorContent[];
+}
+
+interface TutorContent {
+  id: number;
+  type: string;
+  link: string;
+  status: string;
+  uploadTimestamp: string;
+  tutorName: string;
+  tutorId: number;
+}
+
+class AdminService {
+  async getTutorContent() {
+    const url = '/api/v1/admin/tutor-content';
+    const response = await httpService.get<TutorContents>(url);
+    return response;
+  }
+}
+
+const adminService = new AdminService();
+export { AdminService, adminService };
